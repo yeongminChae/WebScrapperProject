1fc7544fcfb7047b28a233efca2bb5de05e7b4bb
채영민 <94206927+yeongminChae@users.noreply.github.com>
2022-06-23 10:33:11 +0000

    first chapter : install the flask

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from flask import Flask
+
 """
 These are the URLs that will give you remote jobs for the word 'python'
 
@@ -7,3 +9,15 @@ https://remoteok.io/remote-dev+python-jobs
 
 Good luck!
 """
+
+app = Flask("SuperScrapper")
+
+@app.route("/")
+def home():
+    return "Hello! welcome to mi casa"
+
+@app.route("/contact")
+def contact() :
+  return "Contact me!"
+
+app.run(host="0.0.0.0")
