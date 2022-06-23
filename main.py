550cde2f9987e5df6fa0e32889c36937c6d09eb5
채영민 <94206927+yeongminChae@users.noreply.github.com>
2022-06-23 09:54:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+These are the URLs that will give you remote jobs for the word 'python'
+
+https://stackoverflow.com/jobs?r=true&q=python
+https://weworkremotely.com/remote-jobs/search?term=python
+https://remoteok.io/remote-dev+python-jobs
+
+Good luck!
+"""
